d8616aed21bc5cb2c941f0fa2f6c5f32d53fac9b
Pierre Rolland <roll.pierre@gmail.com>
2020-05-17 15:15:01 +0200

    Added dist homepage

--- dist/pages/HomePage.js
+++ dist/pages/HomePage.js
@@ -5,19 +5,21 @@ import filterFormDefinitionsHelper from '../helpers/filterFormDefinitionsHelper'
 import './HomePage.css';
 export default (props => {
   const definitions = filterFormDefinitionsHelper(props.definitions, props.withAuth, props.user);
-  const IconComponent = d.icon || /*#__PURE__*/React.createElement(React.Fragment, null);
   return /*#__PURE__*/React.createElement("section", {
     className: "HomePage"
   }, /*#__PURE__*/React.createElement("div", {
     className: "grid-8-small-2-tiny-2"
-  }, definitions.map((d, i) => /*#__PURE__*/React.createElement(Link, {
-    className: "HomePage-link",
-    to: d.baseUrl,
-    key: i
-  }, /*#__PURE__*/React.createElement(Paper, {
-    className: "HomePage-paper",
-    square: true
-  }, /*#__PURE__*/React.createElement("div", {
-    className: "HomePage-linkContent"
-  }, /*#__PURE__*/React.createElement(IconComponent, null), /*#__PURE__*/React.createElement("br", null), d.titles.list))))));
+  }, definitions.map((d, i) => {
+    const IconComponent = /*#__PURE__*/React.createElement(React.Fragment, null, d.icon) || /*#__PURE__*/React.createElement(React.Fragment, null);
+    return /*#__PURE__*/React.createElement(Link, {
+      className: "HomePage-link",
+      to: d.baseUrl,
+      key: i
+    }, /*#__PURE__*/React.createElement(Paper, {
+      className: "HomePage-paper",
+      square: true
+    }, /*#__PURE__*/React.createElement("div", {
+      className: "HomePage-linkContent"
+    }, /*#__PURE__*/React.createElement(IconComponent, null), d.titles.list)));
+  })));
 });

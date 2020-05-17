c568070bb3eb897cd64c9eabb6a2c018a4cfdbf1
Pierre Rolland <roll.pierre@gmail.com>
2020-05-17 23:36:16 +0200

    Enhancement to Input component

--- src/lib/components/Input.jsx
+++ src/lib/components/Input.jsx
@@ -3,5 +3,6 @@ import TextField from '@material-ui/core/TextField';
 
 export default props => <TextField
     {...props}
+    style={{ width: '100%', margin: '0.5rem', ...props.style }}
     onChange={e => props.onChange(e.target.value)}
 />;
